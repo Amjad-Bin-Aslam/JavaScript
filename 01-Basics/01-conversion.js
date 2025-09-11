7ab2b4aa2d572dbfaed7bc5d05e15ca05241381b
Amjad-Bin-Aslam <amjadbinaslam604@gmail.com>
2025-09-11 18:10:03 +0500

    conversion

---
+++ 01-Basics/01-conversion.js
@@ -0,0 +1,17 @@
+const score = 33
+
+console.log(typeof(score))
+
+const changeIntoString = String(score)
+
+console.log(typeof(changeIntoString))
+
+//Number to Bolean conversion
+
+let isLoggedIn = 1
+
+
+let booleanIsloggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsloggedIn)
+
