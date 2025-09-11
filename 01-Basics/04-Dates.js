d7d837f792a3055671340b310869294f376c3c82
Amjad-Bin-Aslam <amjadbinaslam604@gmail.com>
2025-09-12 00:18:17 +0500

    added

---
+++ 01-Basics/04-Dates.js
@@ -0,0 +1,16 @@
+const mydate = new Date()
+// console.log(mydate.toLocaleString())
+// console.log(typeof mydate)
+
+const createdDate = new Date(2025 , 0 , 15)
+// console.log(createdDate.toDateString())
+
+let timeStamp = Date.now()
+// console.log(timeStamp)
+
+let newDate = new Date()
+// console.log(newDate)
+// console.log(newDate.getMonth() + 1)
+
+// console.log(`The date is ${newDate.getDate()} and the time is ${newDate.getTime()}`)
+
