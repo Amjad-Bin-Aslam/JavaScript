e6fdf7d21ac3c5965eff91e7ea3275d7ef51170a
Amjad-Bin-Aslam <amjadbinaslam604@gmail.com>
2025-09-13 00:47:58 +0500

    array

---
+++ 02-Basics/02-arrays.js
@@ -0,0 +1,30 @@
+const hero1 = ["thor" , "ironman" , "spiderman"]
+
+const hero2 = ["superman" , "batman" , "flash"]
+
+// hero1.push(hero2)
+
+// console.log(hero1)
+// console.log(hero1[3])
+
+// const newHero = hero1.concat(hero2);
+// console.log(newHero)
+
+// spread operator
+// const newHero = [...hero1 , ...hero2]
+// console.log(newHero)
+
+// converting to array
+// console.log(Array.isArray("Amjad"))
+// console.log(Array.from("Amjad"))
+
+
+// console.log(Array.from({ name: "amjad" }))
+
+// converting a object into a array
+const obj = { name: "amjad" }
+const arr = Object.keys(obj)
+console.log(arr)
+
+console.log(Array.from(obj))
+
