783b331a01145ee95ecc9911e39fdcb7656a6a3d
Amjad-Bin-Aslam <amjadbinaslam604@gmail.com>
2025-09-16 00:57:54 +0500

    added

--- 03-Basics/03-iifi.js
+++ 03-Basics/03-iifi.js
@@ -4,4 +4,17 @@
 
 ( () => {
     console.log("DB 2")
-} )()
+} )() 
+
+
+let val1 = 10 
+let val2 = 5
+function addNum(num1 , num2){
+    let total = num1 + num2
+    return total
+}
+let result1 = addNum(val1 , val2)
+console.log(result1)
+let result2 = addNum(10 , 2)
+console.log(result2)
+
